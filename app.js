3a7cafdb5349a75f9ae8e55b87e407cb360d0555
andy <202007621@post.au.dk>
2025-08-16 14:33:11 +0200

    Written the most important parts of app.js to make it functional

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 import 'dotenv/config';
 import express from 'express';
+import { verifyKeyMiddleware } from 'discord-interactions';
 import { DiscordRequest } from './utils.js';
 
 // Create an express app
 const app = express();
 // Get port, or default to 3000
 const PORT = process.env.PORT || 3000;
+
+/**
+ * Interactions endpoint URL where Discord will send HTTP requests
+ * Parse request body and verifies incoming requests using discord-interactions package
+ */
+app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async function (req, res) {
+  // Interaction id, type and data
+  const { id, type, data } = req.body;
+
+  /**
+   * Handle verification requests
+   */
+  if (type === InteractionType.PING) {
+    return res.send({ type: InteractionResponseType.PONG });
+  }
+
+  /**
+   * Handle slash command requests
+   * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
+   */
+});
+
+app.listen(PORT, () => {
+  console.log('Listening on port', PORT);
+});
