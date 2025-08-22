9177c5ecddfd086bdc531f585a9b81b228f26955
andy <202007621@post.au.dk>
2025-08-22 17:02:22 +0200

    Added the roll command to the interactions.js which can now be verified on Discord

--- api/interactions.js
+++ api/interactions.js
@@ -32,11 +32,44 @@ export default async function handler(req, res) {
   }
 
   const body = JSON.parse(rawBody);
+  const { data } = body
 
   if (body.type === InteractionType.PING) {
     return res.status(200).json({ type: InteractionResponseType.PONG });
   }
 
+  if (type === InteractionType.APPLICATION_COMMAND) {
+      const {name, options} = data;
+      const {user} = member;
+      const {global_name} = user;
+  
+      let value;
+      if (options == undefined) {
+          // Default maximum value the dice can roll.
+          value = 100;
+      }
+      else {
+          // Use the specified max value given by user.
+          value = options[0].value;
+      }
+  
+      if (name === 'roll') {
+          return res.send({
+              type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+              data: {
+                  flags: InteractionResponseFlags.IS_COMPONENTS_V2,
+                  components: [
+                      {
+                          type: MessageComponentTypes.TEXT_DISPLAY,
+                          content: `${global_name} rolls a ${roll(value)}.`
+                      }
+                  ]
+              }
+          });
+      }
+    }
+
   // Handle other commands here
-  return res.status(200).json({});
+    console.error(`unknown command: ${name}`);
+    return res.status(400).json({error: 'unknown command'});
 }
